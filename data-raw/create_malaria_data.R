a0a427a61aa6dc93d30055d532af97884729f12c
Roman Popat <roman@datakitchen.co.uk>
2018-05-28 17:27:45 +0200

    started converting practical to for malaria dataset

--- data-raw/create_malaria_data.R
+++ data-raw/create_malaria_data.R
@@ -1,2 +1,6 @@
+library(dplyr)
 malaria = read.csv("data-raw/infectious.data.csv")
-devtools::use_data(malaria)
+malaria = malaria %>%
+  group_by(Block, Temperature, Day, Cup) %>%
+  summarise(Sporozoite.Prevalence = mean(Sporozoite.Prevalence))
+devtools::use_data(malaria, overwrite = TRUE)
