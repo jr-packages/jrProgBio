b5cfc5af5cb03eeed10ea78d5df27396def018e5
Roman Popat <roman@datakitchen.co.uk>
2018-05-30 12:05:53 +0200

    incorporating suggestions from issue #3

--- data-raw/create_malaria_data.R
+++ data-raw/create_malaria_data.R
@@ -1,6 +1,9 @@
 library(dplyr)
+
 malaria = read.csv("data-raw/infectious.data.csv")
+
 malaria = malaria %>%
   group_by(Block, Temperature, Day, Cup) %>%
-  summarise(Sporozoite.Prevalence = mean(Sporozoite.Prevalence))
+  summarise(Sporozoite_Prevalence = mean(Sporozoite.Prevalence))
+
 devtools::use_data(malaria, overwrite = TRUE)
